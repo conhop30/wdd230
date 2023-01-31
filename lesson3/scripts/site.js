fe1fa299674db57f259a5ccd9385978dc0242987
conhop30 <conhop30@gmail.com>
2023-01-31 10:01:57 -0700

    Slight Changes

--- lesson3/scripts/site.js
+++ lesson3/scripts/site.js
@@ -2,3 +2,12 @@
 
 const today = new Date();
 document.querySelector("#currentyear").textContent = today.getFullYear();
+
+// Hamburger menu
+function toggleMenu() {
+    document.querySelector("#navbar").classList.toggle('menu-active')
+    document.querySelector("#menu-close").classList.toggle('menu-active')
+    document.querySelector("#menu-open").classList.toggle('menu-active')
+}
+
+document.querySelector("#hamburger-menu").onclick = toggleMenu;
